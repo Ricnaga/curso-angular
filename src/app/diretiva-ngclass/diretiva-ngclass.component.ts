b84909605d7bdfc3d19ff12efae93fb71c0f058c
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-02-17 01:02:11 -0300

    implementation: directive - ngclass

---
+++ src/app/diretiva-ngclass/diretiva-ngclass.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngclass',
+  templateUrl: './diretiva-ngclass.component.html',
+  styleUrls: ['./diretiva-ngclass.component.scss']
+})
+export class DiretivaNgclassComponent {
+  deveSerVerde = false;
+
+  tornarVerde() {
+    this.deveSerVerde = true;
+  }
+}

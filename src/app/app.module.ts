3e6c0dd263ae5c7c56c5f8c2f4d17fc509240383
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-02-17 14:52:38 -0300

    create share folder:apply business rules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 import { HomeComponent } from './home/home.component';
-import { DataBindingExecComponent } from './data-binding-exec/data-binding-exec.component';
-import { ContadorComponent } from './contador/contador.component';
-import { ExerciseExplainedComponent } from './exercise-explained/exercise-explained.component';
-import { ExecDiretivasComponent } from './exec-diretivas/exec-diretivas.component';
+import { DataBindingExecComponent } from './shared/components/data-binding-exec/data-binding-exec.component';
+import { ContadorComponent } from './shared/components/contador/contador.component';
+import { ExerciseExplainedComponent } from './shared/components/exercise-explained/exercise-explained.component';
+import { ExecDiretivasComponent } from './shared/components/exec-diretivas/exec-diretivas.component';
 import { MemesComponent } from './memes/memes.component';
 import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
-import { PipeExecComponent } from './pipe-exec/pipe-exec.component';
+import { PipeExecComponent } from './shared/components/pipe-exec/pipe-exec.component';
 import localeBR from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
 

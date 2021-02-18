964a91fede252eff9c456dce86ef11c01618151d
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-02-17 23:34:46 -0300

    implementation: shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,29 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { HeaderComponent } from './shared/components/header/header.component';
 import { HomeComponent } from './home/home.component';
-import { DataBindingExecComponent } from './shared/components/data-binding-exec/data-binding-exec.component';
-import { ContadorComponent } from './shared/components/contador/contador.component';
-import { ExerciseExplainedComponent } from './shared/components/exercise-explained/exercise-explained.component';
-import { ExecDiretivasComponent } from './shared/components/exec-diretivas/exec-diretivas.component';
-import { MemesComponent } from './memes/memes.component';
-import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
-import { PipeExecComponent } from './shared/components/pipe-exec/pipe-exec.component';
-import localeBR from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { ExtratoComponent } from './extrato/extrato.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { SharedModule } from './shared/shared.module';
+import localeBR from '@angular/common/locales/pt';
 
 registerLocaleData(localeBR, 'pt')
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     HomeComponent,
-    FooterComponent,
-    DataBindingExecComponent,
-    ContadorComponent,
-    ExerciseExplainedComponent,
-    ExecDiretivasComponent,
-    MemesComponent,
-    DiretivaNgclassComponent,
-    PipeExecComponent,
     LoginComponent,
     ExtratoComponent,
     NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [{
     provide:LOCALE_ID,
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DataBindingExecComponent } from './components/data-binding-exec/data-binding-exec.component';
+import { ContadorComponent } from './components/contador/contador.component';
+import { ExerciseExplainedComponent } from './components/exercise-explained/exercise-explained.component';
+import { ExecDiretivasComponent } from './components/exec-diretivas/exec-diretivas.component';
+import { MemesComponent } from '../memes/memes.component';
+import { DiretivaNgclassComponent } from '../diretiva-ngclass/diretiva-ngclass.component';
+import { PipeExecComponent } from './components/pipe-exec/pipe-exec.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    DataBindingExecComponent,
+    ContadorComponent,
+    ExerciseExplainedComponent,
+    ExecDiretivasComponent,
+    MemesComponent,
+    DiretivaNgclassComponent,
+    PipeExecComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports:[
+    HeaderComponent,
+    FooterComponent,
+    DataBindingExecComponent,
+    ContadorComponent,
+    ExerciseExplainedComponent,
+    ExecDiretivasComponent,
+    MemesComponent,
+    DiretivaNgclassComponent,
+    PipeExecComponent,
+  ]
+
+})
+export class SharedModule { }
